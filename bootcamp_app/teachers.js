f93f9d2dd72d4e9f8f2e358432b8000811d24013
Krisan Manoharan <krisan1010@hotmail.com>
2024-06-11 21:26:31 -0400

    Fix teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,25 +6,6 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
- 
-// pool
-//   .query(`
-// SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-// FROM teachers
-// JOIN assistance_requests ON teacher_id = teachers.id
-// JOIN students ON student_id = students.id
-// JOIN cohorts ON cohort_id = cohorts.id
-// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-// ORDER BY teacher;
-//   `)
-//   .then((res) => {
-//     res.rows.forEach((user) => {
-//       console.log(
-//         `${row.cohort}: ${row.teacher}`
-//       );
-//     });
-//   })
-//   .catch((err) => console.error("query error", err.stack));
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -39,4 +20,5 @@ ORDER BY teacher;
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch((err) => console.error("query error", err.stack));
